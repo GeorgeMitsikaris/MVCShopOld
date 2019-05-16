c6e2ea27f5a46bbb60ef64a9d06e6dea1a96fa58
George Mitsikaris <30596553+GeorgeMits@users.noreply.github.com>
2019-05-16 20:13:48 +0300

    Create storage

--- MyShop.DataAccess.InMemory/ProductRepository.cs
+++ MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -1,12 +1,69 @@
-﻿using System;
+﻿using MyShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyShop.DataAccess.InMemory
 {
-    class ProductRepository
+    public class ProductRepository
     {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products;
+
+        public ProductRepository()
+        {
+            products = cache["Products"] as List<Product>;
+            if (products == null)
+                products = new List<Product>();
+        }
+
+        public void Commit()
+        {
+            cache["Products"] = products;
+        }
+
+        public void Insert(Product product)
+        {
+            products.Add(product);
+        }
+
+        public void Update(Product product)
+        {
+            var productFromMemory = products.Find(p => p.Id.Equals(product.Id));
+
+            if (productFromMemory == null)
+                throw new Exception("Product not found");
+            else
+                productFromMemory = product;
+        }
+
+        public Product Find(string id)
+        {
+            var product = products.Find(p => p.Id.Equals(id));
+
+            if (product == null)
+                throw new Exception("Product not found");
+            else
+                return product;
+        }
+
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+
+        public void Delete(string id)
+        {
+            var product = products.Find(p => p.Id.Equals(id));
+
+            if (product == null)
+                throw new Exception("Product not found");
+            else
+                products.Remove(product);
+        }
+
     }
 }
