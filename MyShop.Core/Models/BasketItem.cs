9fa86fd95db5d455d5516b5c75622eb88100f3eb
George Mitsikaris <30596553+GeorgeMits@users.noreply.github.com>
2019-05-28 16:53:13 +0300

    Added Basket Model

---
+++ MyShop.Core/Models/BasketItem.cs
@@ -0,0 +1,10 @@
+﻿namespace MyShop.Core.Models
+{
+    public class BasketItem
+    {
+        public int Id { get; set; }
+        public string BasketId { get; set; }
+        public string ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
